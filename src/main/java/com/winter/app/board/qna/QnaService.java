1e90b1565932d38b81f8cd816377f6e90def307e
js <joonsoo6500@gmail.com>
2025-08-08 17:24:41 +0900

    88

--- src/main/java/com/winter/app/board/qna/QnaService.java
+++ src/main/java/com/winter/app/board/qna/QnaService.java
@@ -87,6 +87,13 @@ public class QnaService implements BoardService{
 			//1. File을 HDD에 저장
 			String fileName = fileManager.fileSave(upload+board, m);
 			
+			//2. 저장된 파일의 정보를 DB에 저장
+			BoardFileVO vo = new BoardFileVO();
+			vo.setOriName(m.getOriginalFilename());
+			vo.setSaveName(fileName);
+			vo.setBoardNum(boardVO.getBoardNum());
+			result = qnaDAO.insertFile(vo);
+		
 			
 		}
 		
@@ -96,14 +103,28 @@ public class QnaService implements BoardService{
 
 	@Override
 	public int delete(BoardVO boardVO) throws Exception {
-		int result = qnaDAO.delete(boardVO);
+		boardVO = qnaDAO.detail(boardVO);
+		
+		for(BoardFileVO vo : boardVO.getBoardFileVOs()) {
+			fileManager.fileDelete(upload+board, vo.getSaveName());
+		}
+		
+		int result = qnaDAO.fileDelete(boardVO);
+		
+		result = qnaDAO.delete(boardVO);
 		return result;
 	}
 
 	@Override
 	public int fileDelete(BoardFileVO boardFileVO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		//1. File 조회
+		boardFileVO = qnaDAO.fileDetail(boardFileVO);
+		//2. File 삭제
+		boolean result =fileManager.fileDelete(upload+board, boardFileVO.getSaveName());
+				
+		//3. DB 삭제
+				
+		return qnaDAO.fileDeleteOne(boardFileVO);
 	}
 
 	
